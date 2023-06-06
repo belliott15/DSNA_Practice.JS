7ba45eee00e29be7ec324b11ef6a95856ba7c1ab
Beau Elliott <belliott1516@gmail.com>
2023-06-06 15:26:41 -0700

    added testing for binary search

---
+++ Searching_Algorithms/binary_search.js
@@ -0,0 +1,26 @@
+//Function accepts a sorted array and a value
+//create a left pointer and a right pointer for a start and end of the array
+//create a middle pointer and if the value is too small move the left pointer up
+//if the value is too large move the right pointer down
+//if the value is never found return -1
+
+function binarySearch(arr, val) {
+  if (arr.length < 1) {
+    return -1;
+  }
+  let left = 0;
+  let right = arr.length - 1;
+  let middle = Math.floor((left + right) / 2);
+
+  while (arr[middle] !== val && left <= right) {
+    if (val < arr[middle]) {
+      right = middle - 1;
+    } else {
+      left = middle + 1;
+    }
+    middle = Math.floor((left + right) / 2);
+  }
+  return arr[middle] === val ? middle : -1;
+}
+
+module.exports = binarySearch;
---
+++ Searching_Algorithms/binary_search.test.js
@@ -0,0 +1,24 @@
+const binarySearch = require("./binary_search.js");
+
+test("function that accepts an array and a value and if the value is in the array return index else return -1", () => {
+  expect(
+    binarySearch(
+      [
+        1, 4, 5, 6, 7, 8, 9, 10, 12, 16, 17, 22, 25, 26, 27, 27, 47, 48, 58, 59,
+        72,
+      ],
+      10
+    )
+  ).toBe(7);
+  expect(
+    binarySearch(
+      [
+        1, 4, 5, 6, 7, 8, 9, 10, 12, 16, 17, 22, 25, 26, 27, 27, 47, 48, 58, 59,
+        72,
+      ],
+      100
+    )
+  ).toBe(-1);
+
+  expect(binarySearch([], 8)).toBe(-1);
+});
