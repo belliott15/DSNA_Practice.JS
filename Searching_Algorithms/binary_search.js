6bbd0a4ed32291fe0a89400a60f9abef4b658253
Beau Elliott <belliott1516@gmail.com>
2023-06-06 15:29:30 -0700

    refactored binary search

--- Searching_Algorithms/binary_search.js
+++ Searching_Algorithms/binary_search.js
@@ -4,21 +4,40 @@
 //if the value is too large move the right pointer down
 //if the value is never found return -1
 
+// function binarySearch(arr, val) {
+//   if (arr.length < 1) {
+//     return -1;
+//   }
+//   let left = 0;
+//   let right = arr.length - 1;
+//   let middle = Math.floor((left + right) / 2);
+
+//   while (arr[middle] !== val && left <= right) {
+//     if (val < arr[middle]) {
+//       right = middle - 1;
+//     } else {
+//       left = middle + 1;
+//     }
+//     middle = Math.floor((left + right) / 2);
+//   }
+//   return arr[middle] === val ? middle : -1;
+// }
+
+//refactored
+
 function binarySearch(arr, val) {
   if (arr.length < 1) {
     return -1;
   }
-  let left = 0;
-  let right = arr.length - 1;
-  let middle = Math.floor((left + right) / 2);
 
-  while (arr[middle] !== val && left <= right) {
-    if (val < arr[middle]) {
-      right = middle - 1;
-    } else {
-      left = middle + 1;
-    }
-    middle = Math.floor((left + right) / 2);
+  let start = 0;
+  let end = arr.length - 1;
+  let middle = Math.floor((start + end) / 2);
+
+  while (arr[middle] !== val && start <= end) {
+    if (val < arr[middle]) end = middle - 1;
+    else start = middle + 1;
+    middle = Math.floor((start + end) / 2);
   }
   return arr[middle] === val ? middle : -1;
 }
